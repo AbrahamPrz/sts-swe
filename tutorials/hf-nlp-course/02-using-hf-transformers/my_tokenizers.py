23028362abd9513408b6cdc16f73c3a32bfee719
abraham <fcoabrahamprz@gmail.com>
2023-04-18 09:05:38 -0700

    Se agregan comentarios para identificar acciones

--- tutorials/hf-nlp-course/02-using-hf-transformers/my_tokenizers.py
+++ tutorials/hf-nlp-course/02-using-hf-transformers/my_tokenizers.py
@@ -7,9 +7,17 @@ def main(args=None):
     tokenizer = AutoTokenizer.from_pretrained(MODEL_CHOICE)
 
     sequence = "Usar los tokenizers de Hugging Face es muy fácil."
+    # tokenization
     tokens = tokenizer.tokenize(sequence)
-
     print(tokens)
+    
+    # conversion to IDs
+    ids = tokenizer.convert_tokens_to_ids(tokens)
+    print(ids)
+    
+    # converting IDs back to a string
+    decoded_string = tokenizer.decode(ids)
+    print(decoded_string)
 
 
 if __name__ == '__main__':
